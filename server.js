f33c1930cce1e59c6e1d093347b14a05f07dee25
sameed <sameedulhassan2@gmail.com>
2020-10-10 19:41:02 +0500

    created routes in seprate file and their controller functions

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+// Route Files
+const bootcamps = require("./routes/bootcamps");
 
 // Load env vars
-
 dotenv.config({ path: "./config/config.env" });
-
-const app = express();
 const PORT = process.env.PORT || 5000;
+const app = express();
 
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
 app.listen(PORT, () => {
     console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
