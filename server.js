9a5cc945766594910239829aa510c2b7f246adf9
sameed <sameedulhassan2@gmail.com>
2020-10-11 16:46:20 +0500

    installed morgan package

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+// Middlewares
+const morgan = require("morgan");
 // Route Files
 const bootcamps = require("./routes/bootcamps");
 
@@ -7,6 +9,11 @@ const bootcamps = require("./routes/bootcamps");
 dotenv.config({ path: "./config/config.env" });
 const PORT = process.env.PORT || 5000;
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
