a74fa9058ea5557c6b447f3b12ee6242ac001a17
sameed <sameedulhassan2@gmail.com>
2020-10-11 20:18:05 +0500

    integrated mongoose and created model for bootcamps

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const colors = require("colors");
+const connectDB = require("./config/db");
 // Middlewares
 const morgan = require("morgan");
 // Route Files
@@ -10,6 +12,8 @@ dotenv.config({ path: "./config/config.env" });
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+// Connect to database
+connectDB();
 // Dev logging middleware
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
@@ -17,6 +21,15 @@ if (process.env.NODE_ENV === "development") {
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
-app.listen(PORT, () => {
-    console.log(`App is running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+const server = app.listen(PORT, () => {
+    console.log(
+        `App is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+    );
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error : ${err.message}`);
+    // Close server
+    server.close(() => process.exit(1));
 });
